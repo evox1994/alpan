37d7fdc7fb005612b8c67551fc15ce842381430c
evox1994 <dmitrybatukhtin@gmail.com>
2020-06-02 16:41:45 +0700

    sdfvdfv

--- js/header.js
+++ js/header.js
@@ -17,4 +17,22 @@ $(document).ready(function(){
 		$('body').removeClass('no-scroll');
 	});
 
+	$(document).on('click','.b-mobile-menu .b-nav li.li-drop span',function(){
+		var eh = $(this).parents('li.li-drop').find('.b-drop-wrap').outerHeight();
+
+		$(this).parents('li.li-drop').toggleClass('active');
+		if ( $(this).parents('li.li-drop').hasClass('active') ){
+			$(this).parents('li.li-drop').find('.b-drop').animate({'height':eh},300);
+		} else {
+			$(this).parents('li.li-drop').find('.b-drop').animate({'height':0},300);
+		}
+	});
+
+	$(window).resize(function(){
+		$('.b-mobile-menu .b-nav li.li-drop.active').each(function(){
+			var eh = $(this).find('.b-drop-wrap').outerHeight();
+			$(this).find('.b-drop').css('height',eh);
+		});
+	});
+
 });
